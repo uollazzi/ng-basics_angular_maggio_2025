8c6880ad02d94ff942569769f598cc1098724f2e
Matteo Valenzi <dilloawallace@hotmail.com>
2025-05-21 13:05:53 +0200

    title component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { ListaProdottiComponent } from "./components/lista-prodotti/lista-prodotti.component";
+import { TitleComponent } from "./components/title/title.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ListaProdottiComponent],
+  imports: [
+    RouterOutlet,
+    // HeaderComponent,
+    FooterComponent,
+    ListaProdottiComponent,
+    TitleComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
